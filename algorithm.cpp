dc0b4f5d6b5ff07ae02b65b70e8e5aa6fa993f41
TremblingOdin <pigmekevin@gmail.com>
2018-10-25 18:00:16 -0400

    Now with comments

--- algorithm.cpp
+++ algorithm.cpp
@@ -12,29 +12,37 @@
 using namespace std;
 
 class Hider {
-
+	//long long is the largest number related data type
+	//No clue how large these will be so need largest possible
 	long long systime;
 	long long dataSize;
 	long long varNumb;
 
 	vector<char> binaryData;
-	char personalData [6] [500];
+	//Only accepting First Name, Last Name, Birthday, Social Security, Address, Primary Car Physician, Medical History
+	//Medical History is limited to 500 characters
+	char personalData [7] [500];
 
+	//Prototypes
 	vector<char> encryptToBinary(char*);
 
+	//Empty Constructor just incase
 	Hider() {
+		//Set up time and other variables
 		time_t tt;
 		systime = * localtime(&tt);
 		dataSize = 0;
-		varNumb = 6;
+		varNumb = 7;
 
 		personalData = {"","","","","",""};
 	}
 
+	//Constructor to accept binary data alongside personal data
 	Hider(const char binary[], char pD[][]) {
+		//Set up time and other variables
 		time_t tt;
 		systime = * localtime(&tt);
-		varNumb = 6;
+		varNumb = 7;
 		dataSize = sizeof(binary);
 
 		personalData = pD;
@@ -42,23 +50,26 @@ class Hider {
 		
 	}
 
+	//if given non-binary data
 	Hider(const char encryption[], char pD[][]) {
+		//Set up time and other variables
 		time_t tt;
 		systime = * localtime(&tt);
-		varNumb = 6;
+		varNumb = 7;
 		dataSize = sizeof(binary);
 
 		personalData = pD;
 		binaryData = encryptToBinary(encryption);
 	}
 	
+	//When given a char array convert it into binary to hide data
 	char[] encryptToBinary(char * data) {
 		//Small 9 characters buffer
 		char output[9];
 
 		//Until the first character pointed by s is not a null character
 		while(*data) {
-			binaryData.push_back(itoa(*data, output, 
+			binaryData.push_back(itoa(*data, output,2); 
 		}
 	}
 }
