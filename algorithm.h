dbec6348fef4d2bdcd1432ad24463abeddb3d97a
DESKTOP-GC95GU0\Fool <reversedfool@hotmail.com>
2018-11-05 20:02:25 -0500

    everything but the Test should be working

--- algorithm.h
+++ algorithm.h
@@ -1,88 +1,109 @@
 /*
- * This file reads in a binary input or converts sent info to binary then randomly hides provided data inside the binary
- * author: Kayvan Ehteshami
- *
- */
+* This file reads in a binary input or converts sent info to binary then randomly hides provided data inside the binary
+* author: Kayvan Ehteshami
+*
+*/
 
+#include "stdafx.h"
 #include <bitset>
 #include <ctime>
+#include <cstring>
 #include <iostream>
 #include <sstream>
 #include <vector>
 
 using namespace std;
 
-class Hider {
-	//long long is the largest number related data type
+//Prototypes
+vector<char> encryptToBinary(char*);
+vector<char> hide();
+void find();
+std::string* getPD();
+vector<char> getBinary();
+vector<char> getNonBinaryData();
+
+class Algorithm {
+public:
+	//long is the largest number related data type
 	//No clue how large these will be so need largest possible
-	long long systime;
-	long long dataSize;
-	long long varNumb;
+	long systime;
+	long dataSize;
+	long varNumb;
 
 	vector<char> binaryData;
-	
-	//Only accepting First Name, Last Name, Birthday, Social Security, Address, Primary Car Physician, Medical History
-	std::string personalData [7];
 
-	//Prototypes
-	vector<char> encryptToBinary(char*);
+	//Only accepting First Name, Last Name, Birthday, Social Security, Address, Primary Car Physician, Medical History
+	std::string personalData[7] = { "" };
 
 	//Empty Constructor just incase
-	Hider() {
+	Algorithm() {
 		//Set up time and other variables
-		time_t tt;
-		systime = * localtime(&tt);
+		tm * tt;
+		systime = mktime(tt);
 		dataSize = 0;
 		varNumb = 7;
-
-		personalData = {"","","","","",""};
 	}
 
 	//Constructor to accept binary data alongside personal data
-	Hider(const std::string binary, std::string pD[]) {
+	Algorithm(const std::string binary, std::string pD[]) {
 		//Set up time and other variables
-		time_t tt;
-		systime = * localtime(&tt);
+		tm * tt;
+		systime = mktime(tt);
 		varNumb = 7;
 		dataSize = sizeof(binary);
 
-		personalData = pD;
-		binaryData = binary;
-		
+		for (int i = 0; i<7; i++)
+			personalData[i] = pD[i];
+
+		//Binary.size() returns unsigned
+		for (unsigned i = 0; i<binary.size(); i++)
+			binaryData.push_back(binary[i]);
+
 	}
 
 	//If given non-binary data
-	Hider(const char * encryption, std::string pd[]) {
+	Algorithm(const char * encryption, std::string pD[]) {
 		//Set up time and other variables
-		time_t tt;
-		systime = * localtime(&tt);
+		tm * tt;
+		systime = mktime(tt);
 		varNumb = 7;
 
-		personalData = pD;
+		for (int i = 0; i<7; i++)
+			personalData[i] = pD[i];
+
 		encryptToBinary(encryption);
 
 		dataSize = sizeof(binaryData);
 	}
-	
+
 	//When given a char array convert it into binary to hide data
-	void encryptToBinary(char * data) {
-		//Small 9 characters buffer
-		char output[9];
+	vector<char> encryptToBinary(const char * data) {
 
 		//Until the first character pointed by s is not a null character
-		while(*data) {
-			binaryData.push_back(itoa(*data, output,2); 
+		while (*data) {
+			binaryData.emplace_back(std::bitset<8>(*data).to_string());
 		}
 	}
 
 	//Hide the data within the binary and return it all
-	char[] hide() {
-		//@ indicates end of a thing of info
-		binaryData.insert(binaryData.begin(), to_string(systime) + "@" + to_string(dataSize) + "@");
+	vector<char> hide() {
+		std::vector<char>::iterator location;
 
-		for(int	i = 0; i < varNumb; i++) {
+		//Char pointer creator
+		const char * binaryPointer = (to_string(systime) + '@' + to_string(dataSize) + '@').c_str();
+		location = binaryData.begin();
+		//@ indicates end of a thing of info
+		while (binaryPointer != NULL) {
+			binaryData.insert(location, *binaryPointer);
+		}
+
+		for (int i = 0; i < varNumb; i++) {
+			binaryPointer = (personalData[i] + '@').c_str();
+
+			long place = (((systime % 10) + i) * (systime % 33)) / 2 + (dataSize / ((systime % 3) + 5));
+
 			//Rudimentary algorithm
-			binaryData.insert((((systime % 10) + i) * (systime%33))/2 + (dataSize/((systime%3)+5)), personalData[i]+ "@");
+			binaryData.insert(location + place, *binaryPointer);
 		}
 	}
 
@@ -91,45 +112,48 @@ class Hider {
 		std::string systimeString = "";
 		std::string dataSizeString = "";
 
-		long long location = 0;		
+		std::vector<char>::iterator location;
+		location = binaryData.begin();
 
-		while(binaryData.at(binaryData.begin())!="@"||binaryData.at(binaryData.begin())!=NULL) {
-			systimeString += binaryData.at(location);
-			binaryData.erase(binaryData.begin());
+		while (binaryData.at(*location) != '@' || binaryData.at(*location) != NULL) {
+			systimeString += binaryData.at(*location);
+			binaryData.erase(location);
 		}
-		binaryData.erase(binaryData.begin());
-		
-		if(binaryData.at(location) == NULL) {
+		binaryData.erase(location);
+
+		if (binaryData.at(*location) == NULL) {
 			throw("Improperly formatted or no data");
 		}
 
 		systime = std::stoll(systimeString);
 
-		while(binaryData.at(binaryData.begin())!="@"||binaryData.at(binaryData.begin())!=Null) {
-			dataSizeString += binaryData.at(location);
-			binaryData.erase(binaryData.begin());
+		while (binaryData.at(*location) != '@' || binaryData.at(*location) != NULL) {
+			dataSizeString += binaryData.at(*location);
+			binaryData.erase(location);
 		}
 		binaryData.erase(binaryData.begin());
 
-		if(binaryData.at(location) == NULL) {
+		if (binaryData.at(*location) == NULL) {
 			throw("Improperly formatted or no data");
 		}
 
 		dataSize = std::stoll(dataSizeString);
 
-		for(int i = 0; i < varNumb; i++) {
-			location = ((systime%10)+i*(systime%33))/2 + (datasize/((systime%3)+1));
-			while(binaryData.at(location)!="@"||binaryData.at(location)!=NULL){
-				personalData[i] += binaryData.at(location);
-				binaryData.erase(location);
+		for (int i = 0; i < varNumb; i++) {
+			long place = ((systime % 10) + i * (systime % 33)) / 2 + (dataSize / ((systime % 3) + 1));
+			while (binaryData.at(*location + place) != '@' || binaryData.at(*location + place) != NULL) {
+				personalData[i] += binaryData.at(*location + place);
+				binaryData.erase(location + place);
 			}
-			binaryData.erase(location);
+			binaryData.erase(location + place);
 		}
 	}
 
 	//Return the personal data
-	std::string[] getPD() {
-		return personalData;
+	std::string* getPD() {
+		std::string* pdp = personalData;
+
+		return pdp;
 	}
 
 	//Return the binary data that originally hid the personal data
@@ -139,18 +163,19 @@ class Hider {
 
 	//Return the original non-binary data
 	vector<char> getNonBinaryData() {
+		std::string binaryString(binaryData.begin(), binaryData.end());
 		//Cpp has so many useful data libraries
-		std::stringstream sstream(binaryData);
-		std::string nonBinaryData;
+		std::stringstream sstream(binaryString);
+		vector<char> nonBinaryData;
 
-		
-		while(sstream.good()){
+
+		while (sstream.good()) {
 			std::bitset<8> bits;
 			sstream >> bits;
 			char c = char(bits.to_ulong());
-			nonBinaryData += c;
+			nonBinaryData.emplace_back(c);
 		}
-		
+
 		return nonBinaryData;
 	}
-}
+};
