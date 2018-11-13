12c027a9bb9223201363bcbcab5a4ffc4434def2
TremblingOdin <pigmekevin@gmail.com>
2018-11-13 17:01:35 -0500

    all errors gone from algorithm.cpp, now only warnings

--- algorithmTest.cpp
+++ algorithmTest.cpp
@@ -5,9 +5,12 @@ using namespace std;
 class AlgorithmTest() {
 	std::string pd[] = ["Jane", "Doe", "4/7/1776", "420696969", "123 Fake Street", "Dr. Acula", "Hemochromatosis, Gaut, Alzheimers' Disorder, Glass Bones Disorder, Paper Skin Disorder, Addicted to Mr. Brightside, Tumblrculosis, Candidiasis Hypersensitivity, Radiiluciussensibilitatem, Situational Depression, Situational Mania, Gluten Allergy"];
 	
-	Algorithm algor = new Algorithm();
+	char binary[] = ['1','0','1','0','1','1','0','0','1','1','1'];	
+
+	Algorithm algor = new Algorithm(pd, binary);
 	
 	void main() {
-		
+		std::cout << algor.getPD();
+		return 0;
 	}
 }
